6f1eef33eda299e1f0391b2a15a48a49ebe10075
Emberlynn Bland <emberlynn.b@gmail.com>
2026-06-26 01:55:39 -0600

    Add teleporter endpoint tool for 3D editor camera

--- classes/Teleporter/teleporter.gd
+++ classes/Teleporter/teleporter.gd
@@ -1,11 +1,11 @@
 @tool
-extends Area3D
+extends Node3D
 
 @export_tool_button("Set End Transform") var end_transform_action = set_end_transform
 
 func set_end_transform():
 	var undo_redo = EditorInterface.get_editor_undo_redo()
 	undo_redo.create_action("Set Teleporter End Position")
-	undo_redo.add_do_property($TargetTransform, &"transform", EditorInterface.get_editor_viewport_3d().get_camera_3d().transform)
-	undo_redo.add_undo_property($TargetTransform, &"transform", transform)
+	undo_redo.add_do_property($TargetTransform, &"global_transform", EditorInterface.get_editor_viewport_3d().get_camera_3d().get_camera_transform())
+	undo_redo.add_undo_property($TargetTransform, &"global_transform", $TargetTransform.global_transform)
 	undo_redo.commit_action()
