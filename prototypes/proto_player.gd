121afb51c06d4866c17af4f0733cee9716bad741
Emberlynn Bland <emberlynn.b@gmail.com>
2026-08-15 23:40:48 -0600

    Make record/playback mutually exclusive
    Also fixed memory not fully resetting on the player's side

--- prototypes/proto_player.gd
+++ prototypes/proto_player.gd
@@ -2,19 +2,43 @@
 class_name ProtoPlayer
 extends CharacterBody2D
 
-# _process() has been hijacked to run the recorder functionality.
-# _physics_process() handles the player's movement and nothing else.
-
-
 ## How fast the player moves, in velocity units.
 const SPEED := 300.0
 ## How fast the memory fills up, as a delta multiplier.
 const MEMORY_SPEED := 0.0
 
-## The recorded notes on the tape recorder.
+## The recorded notes on the tape recorder, pulling from the custom Sound class.
 var memory: Array = []
-## If the recorder is currently accepting new notes.
-var is_recording: bool = false
+
+## Which state the player is currently exhibiting.
+enum STATE {
+	IDLE, ## The player is not recording or playing.
+	RECORDING, ## The player is recording, inputs are added to memory.
+	PLAYING ## The player is playing, new inputs are ignored, world listeners are taking this input now.
+}
+
+var current_state: STATE = STATE.IDLE
+
+## UI for the player to see what they have captured on tape.
+@onready var memory_container_node = %MemoryContainer
+
+## State machine function, sole function is to change state and call related functions.
+func change_state_to(new_state: STATE) -> void:
+	
+	match new_state:
+		STATE.IDLE:
+			current_state = STATE.IDLE
+			%RecordingAnimator.play("stopped")
+			%PlayerSoundSource/AnimationPlayer.play("stopped")
+		STATE.RECORDING:
+			current_state = STATE.RECORDING
+			%RecordingAnimator.play("playing")
+		STATE.PLAYING:
+			current_state = STATE.PLAYING
+			%RecordingAnimator.play("stopped")
+			%PlayerSoundSource.iterator = 0 # set the sequence to play at the start
+			%PlayerSoundSource/AnimationPlayer.play("playing") # start playing the sequence via an animation player loop
+
 # TODO: changeout these two bools to a state machine
 var is_playing: bool = false
 
@@ -23,26 +47,28 @@ var is_playing: bool = false
 func _ready() -> void:
 	%RecorderArea.connect("area_entered", on_recorder_area_entered)
 	%RecorderArea.connect("area_exited", on_recorder_area_exited)
-	%MemoryReset.connect("pressed", set_recording_to_empty)
-	%PlayerSoundSource/AnimationPlayer.play("radar" if is_playing else "RESET")
-	if Engine.is_editor_hint():
-		$CanvasLayer/Debug.show()
+	%MemoryReset.connect("pressed", reset_memory)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed:
 		if event.is_action("proto_startstop_recording"):
-			toggle_recording()
+			match current_state:
+				STATE.RECORDING:
+					change_state_to(STATE.IDLE)
+				STATE.IDLE:
+					change_state_to(STATE.RECORDING)
+				STATE.PLAYING:
+					change_state_to(STATE.RECORDING)
 		if event.is_action("proto_startstop_playback"):
-			toggle_playback()
+			match current_state:
+				STATE.RECORDING:
+					change_state_to(STATE.PLAYING)
+				STATE.IDLE:
+					change_state_to(STATE.PLAYING)
+				STATE.PLAYING:
+					change_state_to(STATE.IDLE)
 
-func _process(delta: float) -> void:
-	if is_recording:
-		if %MemoryFull.value == %MemoryFull.max_value:
-			is_recording = false
-			set_recording_to_full()
-		else:
-			%MemoryFull.value += delta * MEMORY_SPEED
-
+#region Entering/Leaving Other Sound Sources
 func on_recorder_area_entered(area: Area2D) -> void:
 	if area is SoundSource:
 		area.connect("sound_emitted", on_sound_recieved)
@@ -53,54 +79,33 @@ func on_recorder_area_exited(area: Area2D) -> void:
 		if area.is_connected("sound_emitted", on_sound_recieved):
 			area.disconnect("sound_emitted", on_sound_recieved)
 			#print_debug("%s disconnected" % [area])
+#endregion
+
+#region Interpreting Sound Data Inputs
 
 func on_sound_recieved(sound_data: Sound) -> void:
-	if not is_recording:
-		return
-	var distance = global_position.distance_to(sound_data.original_location) / 480.0
-	print_debug("recieved: pitch %s | length %s | octave %s | distance %s !" % [Data.NOTES.keys()[sound_data.pitch], Data.LENGTHS.keys()[sound_data.length], sound_data.octave, distance])
-	add_note_to_memory(sound_data)
+	if current_state == STATE.RECORDING:
+		var distance = global_position.distance_to(sound_data.original_location) / 480.0
+		print_debug("recieved: pitch %s | length %s | octave %s | distance %s !" % [Data.NOTES.keys()[sound_data.pitch], Data.LENGTHS.keys()[sound_data.length], sound_data.octave, distance])
+		add_note_to_memory(sound_data)
 
 func add_note_to_memory(sound_data: Sound) -> void:
 	memory.append(sound_data)
 	var is_long = sound_data.length == Data.LENGTHS.LONG
 	var new_counter: ColorRect = load("res://prototypes/proto_note_%s.tscn" % ["long" if is_long else "short"]).instantiate()
 	new_counter.color = Data.get_note_color(sound_data.pitch)
-	%MemoryContainer.add_child(new_counter)
+	memory_container_node.add_child(new_counter)
 	%PlayerSoundSource.sound_array = [Sound.new(Data.NOTES.HIGHEST, Data.LENGTHS.SHORT, 3, Data.VOLUMES.SILENT)] if memory.is_empty() else memory
 
-func toggle_recording() -> void:
-	is_playing = false
-	is_recording = not is_recording
-	%RecordingAnimator.play("recording" if is_recording else "idle")
-	set_process(is_recording)
-
-func toggle_playback() -> void:
-	is_recording = false
-	is_playing = not is_playing
-	if is_playing:
-		%PlayerSoundSource.iterator = 0
-		%PlayerSoundSource/AnimationPlayer.play("radar")
-	else:
-		%PlayerSoundSource/AnimationPlayer.play("RESET")
-
-func set_recording_to_full() -> void:
-	is_recording = false
-	%RecordingAnimator.play("idle")
-	%MemoryFull.value = %MemoryFull.max_value
-	set_process(false)
-
-func set_recording_to_empty() -> void:
-	# gets priority to avoid out of bounds array race condition
-	%PlayerSoundSource/AnimationPlayer.play("RESET")
-	is_recording = false
-	%RecordingAnimator.play("idle")
-	%MemoryFull.value = 0.0
+func reset_memory() -> void:
+	change_state_to(STATE.IDLE)
+	# BUG: gets priority to avoid out of bounds array race condition OOF
+	# BUG: this is maybe where the array gets confused?
+	memory.clear()
 	%PlayerSoundSource.sound_array = [Sound.new(Data.NOTES.HIGHEST, Data.LENGTHS.SHORT, 3, Data.VOLUMES.SILENT)]
-	set_process(false)
-	for child in %MemoryContainer.get_children():
+	# Handling UI
+	for child in memory_container_node.get_children():
 		child.queue_free()
-
 #endregion
 
 #region Character Movement
@@ -116,8 +121,16 @@ func _physics_process(_delta: float) -> void:
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	move_and_slide()
-	# debug stuff
+#endregion
+
+#region Debug
+
+func _process(_delta: float) -> void:
+	debug_text_display()
+
+func debug_text_display() -> void:
 	var new_string: String = ""
+	new_string += "%s\n" % [STATE.keys()[current_state]]
 	for unit in $PlayerSoundSource.sound_array:
 		new_string += "%s\n" % [Data.NOTES.keys()[unit.pitch]]
 	$CanvasLayer/Debug/Label.text = new_string
